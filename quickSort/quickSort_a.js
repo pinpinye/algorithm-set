ca677d77e42b89c385f1afe4e7df31304e7e468d
wendyye <wendyye@lexinfintech.com>
2018-06-06 20:22:47 +0800

    两种不同方式的排序方式

--- quickSort/quickSort_a.js
+++ quickSort/quickSort_a.js
@@ -1,2 +1,50 @@
 
 // 快速排序 a
+
+let quickSort=(arr,start,end)=>{
+	// 数组为空或者数据的长度为1，直接返回原数组
+	if(arr.length<=1)
+		return arr;
+	// 切分数组,返回一个参照数
+	let flag = partition(arr,start,end);
+	// 对<参照数的数据进行排序
+	if(start<flag-1){
+		quickSort(arr,start,flag-1);
+	}
+	// 对参照数的数据进行排序
+	if(end>flag){
+		quickSort(arr,flag,end);
+	}
+	return arr;
+}
+
+// 切分函数
+let partition = (arr,left,right)=>{
+	// 
+	let l = left;
+	let r = right;
+	if(l >= r)
+		return arr;
+	let mid = arr[Math.floor((r+l)/2)];
+	while(l<=r) {
+		while (arr[l]<mid)
+			l++;
+		while (arr[r]>mid)
+			r--;
+		if(l<=r){
+			let temp;
+			temp = arr[l];
+			arr[l] = arr[r];
+			arr[r] = temp; 
+			l++;
+			r--;
+		}
+	}
+	return l;
+}
+
+
+let a = [3,2,45,5,4545,344,4,1];
+let b;
+b = quickSort(a,0,a.length-1);
+console.log(b);
--- quickSort/quickSort_b.js
+++ quickSort/quickSort_b.js
@@ -1,1 +1,24 @@
-quickSort_b.js
+// 快速排序 
+let quickSort = (arr) => {
+
+	if (arr.length <= 1 )
+			return arr;
+	let left = [];
+	let right = [];
+	let flag = Math.floor(arr.length / 2);
+	// let mid = arr.splice(flag,1)[0];  //--------------1
+	let mid = arr[flag];              //--------------2
+	for(var i = 0; i < arr.length; i++ ) {
+		if(arr[i]< mid){
+			left.push(arr[i])
+		// }else{                           //---------------1
+		}else if(arr[i] > mid){							 //---------------2
+			right.push(arr[i])
+		}
+	};	
+	return quickSort(left).concat([mid],quickSort(right));
+}
+
+let a = [1,4,3,2];
+let b = quickSort(a);
+console.log(b);
