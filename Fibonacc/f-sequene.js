cfd86182118c5e7eaeb2b07cacf65904593c6d72
popy <569772668@qq.com>
2018-07-26 08:42:49 +0800

    fibonaci

--- Fibonacc/f-sequene.js
+++ Fibonacc/f-sequene.js
@@ -14,11 +14,15 @@ function fibonaccOther(n){
 	if (n==1 || n==2) {
 		return  1
 	}
-	let sum = 2;
-	let num = 0;
-	for(let i =3; i<=n ;i++) {
-		sum = sum + num;
-		num = sum;
+	let pre1 = 1;
+	let pre2 = 1;
+	let num;
+	for(let i = 3; i<=n ;i++) {
+		num = pre1 + pre2;
+		if(i!==3 ){
+			pre2 = pre1;
+		 }
+		 pre1 = num;
 	}
-	return sum
+	return num
 }
