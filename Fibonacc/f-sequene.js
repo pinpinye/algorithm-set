9b31d15d1b276cf81d56312f3b343a60767361fa
popy <569772668@qq.com>
2018-07-27 23:30:15 +0800

    array

--- Fibonacc/f-sequene.js
+++ Fibonacc/f-sequene.js
@@ -26,3 +26,18 @@ function fibonaccOther(n){
 	}
 	return num
 }
+
+//数据
+
+function fibonaccOther(n){
+	let arr = [];
+	arr[0] = 0;
+	arr[1] = 1;
+	for (int i=2; i<=n; i++)
+	{
+		arr[i] = arr[i-1] + arr[i-2];
+	}
+	return arr[n];
+}
+
+
