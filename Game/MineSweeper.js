9bf6d8d23644a2e08409578fb41fda0d5e5c60f5
wendyye <wendyye@lexinfintech.com>
2018-08-07 21:05:48 +0800

    fillMine

--- Game/MineSweeper.js
+++ Game/MineSweeper.js
@@ -14,7 +14,7 @@ mineSweeper.prototype = {
 		let m = this.m;
 		let n = this.n;
 		let k = this.k;
-		for(let i=0 ; i<m;i++) {
+		for(let i=0; i<m;i++) {
 			this.rowArr[i] = [];
 			for(let j=0;j<n;j++) {
 				this.rowArr[i][j] = 0;
@@ -36,14 +36,85 @@ mineSweeper.prototype = {
 				console.log(ceil)
 			})
 		})
-	};
+	},
 	fillMine:function() {
-		
+		// 遍历每个格子，避开雷，找出周围的8个空格有几个雷，填数
+		let m = this.m;
+		let n = this.n;
+		for(let i=0; i<m;i++) {
+			for(let j=0;j<n;j++) {
+				let leftTop,toTop,rightTop,toLeft,toRight,leftBottom,rightBottom,toBottom;
+				if(this.rowArr[i][j] == 0 ) {
+					// 第一列
+					if(i==0){
+						leftTop =0;
+						toTop = 0;
+						rightTop = 0;
+					}
+					// 第一行
+					if(j==0) {
+						leftTop =0;
+						toLeft = 0;
+						leftBottom = 0
+					}
+					// 最后一行
+					if(i == this.n-1) {
+						leftBottom = 0;
+						rightBottom = 0;
+						toBottom = 0;
+					}
+					// 最后一列
+					if(j == this.m-1) {
+						rightTop = 0;
+						toRight = 0;
+						rightBottom = 0;
+					}
+
+					if(i>0 && j>0) {
+						leftTop = this.rowArr[i-1][j-1];
+					}
+
+					if(i>0 && j<n-1) {
+						leftBottom = this.rowArr[i-1][j+1];
+					}
+
+					if(j>0 && i<m-1) {
+						rightTop = this.rowArr[i+1][j-1];
+					}
+
+					if(i<m-1 && i<n-1) {
+						rightBottom = this.rowArr[i+1][j+1];
+					}
+	                // ----------------------------------
+					if(i>0) {
+						toLeft = this.rowArr[i-1][j]
+					}
+					if(j>0) {
+						toTop = this.rowArr[i][j-1]
+					}
+					if(i<m-1) {
+						toRight = this.rowArr[i+1][j]
+					}
+					if(j<n-1) {
+						toBottom = this.rowArr[i][j+1]
+					}
+					this.rowArr[i][j] = leftTop+toTop+rightTop+toLeft+toRight+leftBottom+rightBottom+toBottom;
+				}
+
+			}
+		}
+		// 尝试打印
+		this.rowArr.forEach(item => {
+			item.forEach(ceil => {
+				console.log(ceil)
+			})
+		})
 	}
 }
 
 let my =new mineSweeper(5,5,1);
 my.generateFrame();
+my.fillMine();
 
 
 
