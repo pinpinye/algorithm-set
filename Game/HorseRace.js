726d1353d765172d1a51204c4ee2110d20626707
wendyye <wendyye@lexinfintech.com>
2018-08-09 21:03:44 +0800

    horserace

---
+++ Game/HorseRace.js
@@ -0,0 +1,3 @@
+// There are 5 runways and 35 horses，only 5 horses can racing in one round each time.;
+// Asume that each horse run in their stale ability. whthout any tools, how to find the 
+// top 5 horses which run fastest by racing as less  as possibile. 
