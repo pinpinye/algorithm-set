7c135df703483fffdb760fa0c69ecdc89746e27a
blinkpopy <569772668@qq.com>
2019-05-07 15:45:42 +0800

    Create password-verify.js

---
+++ js/password-verify.js
@@ -0,0 +1,58 @@
+let ruleList = ['123456789','abcdefg'];
+
+let count = 0;
+
+let countRepeat = function(password ,rule) {
+     // 创建二维数组
+    let myDoubleArr = []
+    let ruleToArr = rule.split('');
+    let passwordToArr = password.split('');
+    let m = ruleToArr.length;
+    let n = passwordToArr.length;
+    for (let i = 0 ; i < n+1; i ++) {
+        myDoubleArr[i] = []
+    }
+    let row = 0 , col = 1;
+    ruleToArr.forEach(item => {
+        myDoubleArr[row][col] = item;
+        col++;
+    })
+    row = 1; col = 0;
+    passwordToArr.forEach(item => {
+        myDoubleArr[row][col] = item;
+        row++;
+    })
+
+    // 对比两个字符串是否相同，相同则对应坐标赋值为1
+    for(let i = 1 ; i < m+1 ; i++) {
+        for (let j = 1 ; j < n+1 ; j++) {
+            if(myDoubleArr[j][0] === myDoubleArr[0][i] ) {
+                myDoubleArr[j][i] = 1;
+            } else {
+                myDoubleArr[j][i] = 0;
+            }
+        }
+    }
+    // 记录连续次数
+    for(let i = 1 ; i < m-1 ; i++) {
+        for (let j = 1 ; j < n-1 ; j++) {
+            if(myDoubleArr[j][i] === 1 && myDoubleArr[j+1][i+1] === 1 && myDoubleArr[j+2][i+2] === 1) {
+                count ++
+            }
+        }
+    }
+ }
+
+// 对所有规则进行一次计算
+let testBadPassword = function(password) {
+    ruleList.forEach(item => {
+        countRepeat(password, item)
+    })
+    if (count >= 2) {
+        count = 0;
+        return true
+    } else {
+        count = 0;
+        return false
+    }
+}
