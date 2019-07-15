76e7d79e5faad0b420ea8b01c59055861cbf30bc
blinkpopy <569772668@qq.com>
2019-07-15 12:25:33 +0800

    Update f-sequene.js
    update

--- Fibonacc/f-sequene.js
+++ Fibonacc/f-sequene.js
@@ -3,7 +3,7 @@
 
 //递归
 function fibonacc(n){
-	if (n==1 || n==2) {
+	if (n <= 2) {
 		return  1
 	}
 	return fibonacc(n-1) + fibonacc(n-2)
@@ -11,29 +11,27 @@ function fibonacc(n){
 //非递归
 
 function fibonaccOther(n){
-	if (n==1 || n==2) {
-		return  1
+	if (n <= 2) {
+		return  n
 	}
 	let pre1 = 1;
 	let pre2 = 1;
-	let num;
-	for(let i = 3; i<=n ;i++) {
-		num = pre1 + pre2;
-		if(i!==3 ){
-			pre2 = pre1;
-		 }
-		 pre1 = num;
+	let sum;
+	for(let i = 1; i<n-1 ;i++) {
+		 sum = pre1 + pre2;
+		 pre1 = pre2;
+		 pre2 = sum;
 	}
-	return num
+	return sum
 }
 
-//数据
+//数组
 
 function fibonaccOther(n){
 	let arr = [];
-	arr[0] = 0;
-	arr[1] = 1;
-	for (int i=2; i<=n; i++)
+	arr[0] = 0;                                                                                                                                                                                                                        n                                                       
+	arr[1] = 1;	
+	for (let i=2; i<=n; i++)
 	{
 		arr[i] = arr[i-1] + arr[i-2];
 	}
