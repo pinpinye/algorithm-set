019b7ecda2757baba9db3d4773dd01339fa299f3
blinkpopy <569772668@qq.com>
2020-05-18 03:11:03 +0800

    Update debounce.js

--- js/debounce.js
+++ js/debounce.js
@@ -1,1 +1,14 @@
 
+
+function debounce(fn, delay,immediate) {
+	let timer = null;
+	return function(...args) {
+		if(timer)  clearTimeout(timer);
+		if(immediate && !timer) {
+			fn.apply(this,args);
+		} 
+		timer = setTimeout(()=> {
+				fn.apply(this, args);
+ 		}, delay);
+	}
+}
