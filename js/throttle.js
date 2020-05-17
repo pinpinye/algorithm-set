ba0b2d2b6c1e26d69fb71dc03bcc768ce01c0e97
blinkpopy <569772668@qq.com>
2020-05-18 02:11:52 +0800

    Update throttle.js

--- js/throttle.js
+++ js/throttle.js
@@ -1,1 +1,12 @@
 
+
+function throttle(fn, delay) {
+	let lastTime = 0;
+	return function(...args) {
+		let currentTime = new Date();
+		if(currentTime - lastTime >= delay) {
+			fn.apply(this,...args);
+			lastTime = currentTime;
+		}
+	}
+}
