5c010e45108b8343bfaca3510e655e20119662f6
blinkpopy <569772668@qq.com>
2020-06-02 17:59:07 +0800

    Update binarySearch.js

--- Sort/quickSort/binarySearch.js
+++ Sort/quickSort/binarySearch.js
@@ -1,3 +1,4 @@
+// 循环
 var search1 = function(nums, target) {
 
   let low = 0;
@@ -13,6 +14,22 @@ var search1 = function(nums, target) {
       high = mid -1;
     }
   } 
+  return -1 
+  
+};
+
+// 递归
+var search2 = function(nums, target, l, h) {
+  let low = l || 0;
+  let high = h || nums.length -1;
+  let mid = parseInt(low + (high - low) / 2);
+    if(nums[mid] === target) {
+      return mid;
+    } else if(nums[mid] < target) {
+      return search(nums, target, mid + 1, high);
+    } else if(nums[mid] > target) {
+      return search(nums, target, low, mid -1);
+    }
   return -1 
   
 };
